f1278c9a8e7932384a76cad25d23fd6683e808cb
VictorMauroy <mauroy.victor@gmail.com>
2023-05-31 18:55:24 +0200

    feat: :construction: Add a base to change turn and check a box. Add explanation at launch

--- game.py
+++ game.py
@@ -3,9 +3,12 @@ cells_table = [
     ["","x",""],
     ["o","","x"]
 ]
+isBoxTurn:bool
+selectedCell:int
 
 def Show_Cells(cells:list) -> None :
-    """Show each cells of the morpion table in the terminal
+    """Show each cells of the morpion table in the terminal \n
+        Also organize the table of cells to print it
 
     Args:
         cells (list): Table of cells to show
@@ -18,4 +21,27 @@ def Show_Cells(cells:list) -> None :
                 print(character, end=" ")
         print()
 
+def Check_Box(position:int, isBox:bool) -> None :
+    """Check  a box or make a circle in the table of cells with a given position
+
+    Args:
+        position (int): Position of the cell to check
+        isBox (bool): Whether the current turn is for box or circle
+    """
+
+    isBoxTurn = not isBoxTurn
+
+def NextTurn() -> None:
+    selectedCell = input("Veuillez entrer une position de 1 à 9")
+    try:
+        int(selectedCell)
+        #Do the other checks here
+    except:
+        print("Not a correct position, try again.")
+        NextTurn()
+
+print("Afin de jouer, entrez une position de 1 à 9.\n"
+      "Les positions 1 à 3 correspondent aux cases du haut,\n"
+      "les cases 4 à 6 à celles du milieu, etc.")
+isBoxTurn = True
 Show_Cells(cells_table)
