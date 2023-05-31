ee3bac7f44adfee4ee233e7b0b79f03fdfa0aad7
VictorMauroy <mauroy.victor@gmail.com>
2023-05-31 18:32:52 +0200

    feat: :sparkles: improvement of the show table of cells function

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 cells_table = [
-    ["","",""],
-    ["","",""],
-    ["","",""]
+    ["x","","o"],
+    ["","x",""],
+    ["o","","x"]
 ]
 
 def Show_Cells(cells:list) -> None :
@@ -11,6 +11,11 @@ def Show_Cells(cells:list) -> None :
         cells (list): Table of cells to show
     """
     for elt in cells:
-        print(elt)
+        for character in elt:
+            if(character == ""):
+                print(" ", end=" ")
+            else:
+                print(character, end=" ")
+        print()
 
 Show_Cells(cells_table)
