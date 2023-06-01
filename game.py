e8faaa742d0efc68caa4e4c35b9ed390582cb784
VictorMauroy <mauroy.victor@gmail.com>
2023-06-01 09:46:35 +0200

    Feat: :sparkles: :bug: modify the game to ask coordinate instead of a single position and resolve global var errors

--- game.py
+++ game.py
@@ -1,10 +1,9 @@
 cells_table = [
     ["","",""],
     ["","",""],
-    ["","","x"]
+    ["","",""]
 ]
 isBoxTurn = True
-selectedCell:int
 
 #region SHOW and CHECK cells
 def Show_Cells(cells:list) -> None :
@@ -22,44 +21,60 @@ def Show_Cells(cells:list) -> None :
                 print(character, end=" ")
         print()
 
-def Is_Position_Valid(pos_to_check:int) -> None :
-    """Check if the given position is empty in the table of cells"""
-"""
-    for elt in cells_table:
-        for character in elt:
-            if(character == ""):
-                
-            else:"""
-#endregion
-
-def Fill_Cell(isBox:bool) -> None :
-    """Check  a box or make a circle in the table of cells with a given position
+def Is_Position_Valid(pos_to_check:int, isLine:bool) -> None :
+    """_summary_
 
     Args:
-        isBox (bool): Whether the current turn is for box or circle
+        pos_to_check (int): _description_
+        isLine (bool): _description_
     """
-    # Obtention de la ligne et de la colonne à partir du nombre donné -1, puisque les tableaux commencent à 0
-    cells_table[int((selectedCell-1)/3)][(selectedCell-1)%3] = "x" if isBox else "o"
-    isBoxTurn = not isBox
-    print("Joueur CROIX, à vous !" if isBox else "Joueur CERCLE, let's go !")
+    print()
+#endregion
+
+def Fill_Cell(line, column) -> None :
+    """Check  a box or make a circle in the table of cells with 
+        a given line and column
+    """
+    global selectedLine #Mettre en global pour pouvoir utiliser une variable extérieure
+    global isBoxTurn
+
+    # Well, old code without the coordinates system but sorry, I found it interesting
+    # cells_table[int((selectedLine-1)/3)][(selectedLine-1)%3] = "x" if isBoxTurn else "o"
+    
+    cells_table[line-1][column-1] = "x" if isBoxTurn else "o"
+    isBoxTurn = not isBoxTurn
+    print("Joueur CROIX, à vous !" if isBoxTurn else "Joueur CERCLE, let's go !")
     Show_Cells(cells_table)
     NextTurn()
 
 def NextTurn() -> None:
-    selectedCell = input("Veuillez entrer une position de 1 à 9 : ")
+    selectedLine:int
+    selectedColumn:int
+    selectedLine = input("Quelle ligne ? ==> ")
     try:
-        int(selectedCell)
+        # Ne pas oublier d'assigner pour que selectedCell prenne un type "int"
+        selectedLine = int(selectedLine)
         #Do the other checks here
-        #Is_Position_Valid(selectedCell)
+        #Is_Position_Valid(selectedLine, True)
     except:
-        print("Not a correct position, try again.")
+        print("Ceci n'est pas une ligne valide.")
         NextTurn()
-    Fill_Cell(isBoxTurn)
+    
+    selectedColumn = input("Quelle colonne ? ==> ")
+    try:
+        selectedColumn = int(selectedColumn)
+        #Is_Position_Valid(selectedColumn, False)
+    except:
+        print("Ceci n'est pas une cellule valide.:\n"
+              "Renseignez à nouveau ligne et colonne.")
+        NextTurn()
+    # Once everything is good, continue !
+    Fill_Cell(selectedLine, selectedColumn)
 
 ############ Initialization of the game ############ 
-print("Afin de jouer, entrez une position de 1 à 9.\n"
-      "Les positions 1 à 3 correspondent aux cases du haut,\n"
-      "les cases 4 à 6 à celles du milieu, etc.")
+print("Afin de jouer :\n"
+      "Entrez le numéro de la ligne de 1 à 3\n"
+      "puis le numéro de la colonne !")
 
 #Let's say box starts first everytime 
 # and it's up to the user to determine who play first
